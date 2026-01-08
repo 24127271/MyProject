25b2151a0ade2391703d9ec78ea76e25432dc193
Dann <dnydan2411@clc.fitus.edu.vn>
2026-01-08 16:52:33 +0700

    Doi SQL

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Cấu hình Database và Secret Key (bắt buộc để dùng thông báo flash)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'uel_graduation_2026' 
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['SECRET_KEY'] = 'uel_graduation_2026' 
+
+import os
+
+# Code sẽ tự động lấy link từ Render, nếu không có (chạy ở máy) sẽ dùng sqlite
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///database.db')
 
 db = SQLAlchemy(app)
 
